bc3e5e792549179c42a59cf465b32e6f331786b8
kulkaa <v.cepokas@gmail.com>
2026-02-10 20:30:27 +0200

    Update const.py

--- custom_components/xiaomi_home/miot/const.py
+++ custom_components/xiaomi_home/miot/const.py
@@ -132,7 +132,7 @@ DEFAULT_CTRL_MODE: str = 'auto'
 
 # Registered in Xiaomi OAuth 2.0 Service
 # DO NOT CHANGE UNLESS YOU HAVE AN ADMINISTRATOR PERMISSION
-OAUTH_REDIRECT_URL: str = 'http://homeassistant.local:8123'
+OAUTH_REDIRECT_URL: str = 'http://192.168.50.253:8123/'
 
 MIHOME_CA_CERT_STR: str = '-----BEGIN CERTIFICATE-----\n' \
     'MIIBazCCAQ+gAwIBAgIEA/UKYDAMBggqhkjOPQQDAgUAMCIxEzARBgNVBAoTCk1p\n' \
